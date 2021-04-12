fd6f495a610fb11293d7bdda569818402399b517
Emanuele Galli <egalli64@gmail.com>
2021-04-12 22:26:53 +0200

    #8

--- src/main/java/com/example/sws/s12/HelloCtr.java
+++ src/main/java/com/example/sws/s12/HelloCtr.java
@@ -1,5 +1,7 @@
 package com.example.sws.s12;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -8,9 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloCtr {
-    @GetMapping("/s12/hello")
+    private static final Logger log = LogManager.getLogger(HelloCtr.class);
+
+    @GetMapping("/s12/hello1")
     public String hello(Model model) {
+        log.traceEntry("hello");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("name", auth.getName());
+        return "/s12/hello";
+    }
+
+    @GetMapping("/s12/hello2")
+    public String hello(Model model, Authentication auth) {
+        log.traceEntry("hello2");
         model.addAttribute("name", auth.getName());
         return "/s12/hello";
     }
--- src/main/webapp/WEB-INF/jsp/s12/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/s12/hello.jsp
@@ -4,8 +4,12 @@
 <head>
 <meta charset="UTF-8">
 <title>${name} home page</title>
+<link rel="icon" href="data:;base64,=">
+<link rel="stylesheet" type="text/css" href="/css/simple.css">
 </head>
 <body>
     <h1>Hello ${name}!</h1>
+    
+    <p>Back <a href="/">home</a></p>
 </body>
 </html>
