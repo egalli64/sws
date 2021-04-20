c6a1b2a00112ca9578861b400dc526ae69b2451c
Emanuele Galli <egalli64@gmail.com>
2021-04-20 21:17:51 +0200

    cleanup

--- src/main/java/com/example/sws/SwsApplication.java
+++ src/main/java/com/example/sws/SwsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SwsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SwsApplication.class, args);
+    }
 }
