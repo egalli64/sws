6a25a56fa7a79b87fb0e037cd633f112fefb6684
Emanuele Galli <egalli64@gmail.com>
2021-04-20 22:11:26 +0200

    minimized

--- src/main/java/com/example/sws/s07/SimpleUserDetailsService.java
+++ src/main/java/com/example/sws/s07/SimpleUserDetailsService.java
@@ -2,11 +2,14 @@ package com.example.sws.s07;
 
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class SimpleUserDetailsService implements UserDetailsService {
+    private static final Logger log = LogManager.getFormatterLogger(SimpleUserDetailsService.class);
     private Set<UserDetails> users;
 
     public SimpleUserDetailsService(Set<UserDetails> users) {
@@ -15,6 +18,7 @@ public class SimpleUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.traceEntry("loadUserByUsername {}", username);
         return users.stream().filter(u -> u.getUsername().equals(username)).findAny().orElseThrow( //
                 () -> new UsernameNotFoundException("User '" + username + "' not found"));
     }
