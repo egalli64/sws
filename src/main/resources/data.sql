36a0bb5d089608ae41c67b343072ca970a9aedfb
Emanuele Galli <egalli64@gmail.com>
2021-04-21 21:37:19 +0200

    minimized

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,6 +11,3 @@ create table users (
 -- {bcrypt}
 insert into users (username, password) values('tom', '$2a$10$BsXAGpkEe6YRV2KbJ996ReSkflDfZgPxpaDq/6B7Y15nVBT6yuo3W');
 insert into users (username, password, admin) values('bob', '$2a$10$BsXAGpkEe6YRV2KbJ996ReSkflDfZgPxpaDq/6B7Y15nVBT6yuo3W', true);
-
--- {noop}
--- insert into users (username, password) values('tom', 'password');
