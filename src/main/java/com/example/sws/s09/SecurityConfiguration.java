268d9fe791780d77bef4102ce8ce74bfbb26ec87
Emanuele Galli <egalli64@gmail.com>
2021-04-21 20:52:32 +0200

    minimized

--- src/main/java/com/example/sws/s09/SecurityConfiguration.java
+++ src/main/java/com/example/sws/s09/SecurityConfiguration.java
@@ -1,13 +1,13 @@
 package com.example.sws.s09;
 
-//import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 // !!! I'm intentionally using the deprecated NoOpPasswordEncoder !!!
 @SuppressWarnings("deprecation")
-//@Configuration
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private JpaUserDetailsService svc;
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,9 +8,5 @@ create table users (
     admin boolean default false
 );
 
--- {bcrypt}
-insert into users (username, password) values('tom', '$2a$10$BsXAGpkEe6YRV2KbJ996ReSkflDfZgPxpaDq/6B7Y15nVBT6yuo3W');
-insert into users (username, password, admin) values('bob', '$2a$10$BsXAGpkEe6YRV2KbJ996ReSkflDfZgPxpaDq/6B7Y15nVBT6yuo3W', true);
-
 -- {noop}
--- insert into users (username, password) values('tom', 'password');
+insert into users (username, password) values('tom', 'password');
