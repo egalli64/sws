1634e93346f479908e2cc47999cfaa2fe21a1417
Emanuele Galli <egalli64@gmail.com>
2021-05-23 10:57:03 +0200

    #12 var clarified in comment

--- sws05/src/main/java/com/example/sws/SecurityConfiguration.java
+++ sws05/src/main/java/com/example/sws/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.AuthorizedUrl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -27,6 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         log.traceEntry("custom.security.configuration " + customSecurityConfiguration);
 
+        // ExpressionUrlAuthorizationConfigurer<HttpSecurity>.AuthorizedUrl
         var authorizedAny = http.authorizeRequests().anyRequest();
 
         switch (customSecurityConfiguration) {
