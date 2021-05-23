46c626840421f42dd07d3ddb779feedc5fdc7251
Emanuele Galli <egalli64@gmail.com>
2021-05-23 10:51:25 +0200

    #12

--- sws05/src/main/java/com/example/sws/SecurityConfiguration.java
+++ sws05/src/main/java/com/example/sws/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.sws;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.AuthorizedUrl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -12,27 +16,33 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @SuppressWarnings("deprecation")
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final Logger log = LogManager.getLogger(SecurityConfiguration.class);
+    @Value("${custom.security.configuration}")
+    private String customSecurityConfiguration;
+
     /**
      * Variations on WebSecurityConfigurerAdapter standard implementation
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // default configuration
-//        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+        log.traceEntry("custom.security.configuration " + customSecurityConfiguration);
 
-        /*
-         * when no formLogin() is specified 
-         * 
-         * no Spring login form, the browser default is used instead
-         * 
-         * _and_ no login / logout pages available
-         */
+        var authorizedAny = http.authorizeRequests().anyRequest();
 
-        // !!! Useless - basic HTTP authentication is used but any request by anyone is authorized !!!
-//        http.authorizeRequests().anyRequest().permitAll().and().httpBasic();
-
-        // default behavior, any request should be authenticated (but no login/logout pages!)
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        switch (customSecurityConfiguration) {
+        case "useless":
+            // !!! basic HTTP authentication but any request by anyone is authorized !!!
+            authorizedAny.permitAll().and().httpBasic();
+            break;
+        case "noForm":
+            // any request should be authenticated - but no login/logout pages!
+            authorizedAny.authenticated().and().httpBasic();
+            break;
+        default:
+            // default Spring Security configuration behavior
+            authorizedAny.authenticated().and().formLogin().and().httpBasic();
+            break;
+        }
     }
 
     @Override
