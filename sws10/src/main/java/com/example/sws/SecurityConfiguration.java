5357a24919ad271fa5cb96f1c3896276033f682d
Emanuele Galli <egalli64@gmail.com>
2021-05-23 17:44:24 +0200

    #13

--- sws10/src/main/java/com/example/sws/SecurityConfiguration.java
+++ sws10/src/main/java/com/example/sws/SecurityConfiguration.java
@@ -3,18 +3,20 @@ package com.example.sws;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private JpaUserDetailsService svc;
-
-    public SecurityConfiguration(JpaUserDetailsService svc) {
-        this.svc = svc;
-    }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(svc).passwordEncoder(new BCryptPasswordEncoder());
+        var manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("tom") //
+                .password("$2a$10$BsXAGpkEe6YRV2KbJ996ReSkflDfZgPxpaDq/6B7Y15nVBT6yuo3W") //
+                .authorities("user").build());
+
+        auth.userDetailsService(manager).passwordEncoder(new BCryptPasswordEncoder());
     }
+
 }
