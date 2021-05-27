be0bc8df03b8bb5482d8de53bdefa954243fd77f
Emanuele Galli <egalli64@gmail.com>
2021-05-27 20:43:21 +0200

    OncePerRequestFilter

--- sws16/src/main/java/com/example/sws/ReplacedAuthenticationFilter.java
+++ sws16/src/main/java/com/example/sws/ReplacedAuthenticationFilter.java
@@ -2,35 +2,29 @@ package com.example.sws;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class ReplacedAuthenticationFilter implements Filter {
-    private static final Logger log = LogManager.getLogger(ReplacedAuthenticationFilter.class);
-
+public class ReplacedAuthenticationFilter extends OncePerRequestFilter {
     @Value("${authentication.id}")
     private String authenticationId;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        String authentication = ((HttpServletRequest) request).getHeader("Authentication");
-        log.trace("authentication: " + authentication);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String authentication = request.getHeader("Authentication");
+        logger.trace("authentication: " + authentication);
         if (authenticationId.equals(authentication)) {
             chain.doFilter(request, response);
         } else {
-            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
     }
 }
