8511e12fa5c43ec6fd337ba799f9b15b489e53a3
Emanuele Galli <egalli64@gmail.com>
2021-05-31 18:39:24 +0200

    #19

--- sws18/src/main/java/com/example/sws/SecurityConfiguration.java
+++ sws18/src/main/java/com/example/sws/SecurityConfiguration.java
@@ -1,24 +1,23 @@
 package com.example.sws;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService uds() {
-        var manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("tom") //
-                .password("$2a$10$BsXAGpkEe6YRV2KbJ996ReSkflDfZgPxpaDq/6B7Y15nVBT6yuo3W") //
-                .authorities("ROLE_USER").build());
-        return manager;
+        return new InMemoryUserDetailsManager();
     }
 
     @Bean
@@ -29,5 +28,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+
+        http.cors(c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://localhost:8080"));
+                config.setAllowedMethods(List.of("GET"));
+                return config;
+            };
+            c.configurationSource(source);
+        });
     }
 }
